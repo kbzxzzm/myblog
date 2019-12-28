1f3702af5617851636dc681cf6fa7ce86817134e
kbzxzzm <508153455@qq.com>
2019-12-28 11:19:27 +0800

    我的个人博客创建

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,20 @@ module.exports = {
   extends: ["plugin:vue/essential", "@vue/prettier"],
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    // 匹配规则:[开启规则参数，要求/禁用参数]
+    semi: ["error", "always"], // 简单语句后边要求设置分号，不设置会报错
+    semi: ["error", "never"], // 简单语句后边不要设置分号，设置会报错
+    semi: ["off", "always"], // 关闭规则检测
+    semi: ["off", "never"], // 关闭规则检测
+
+    "space-before-function-paren": ["error", "never"], // 禁止在function小括号左边设置空格，否则报错
+    "space-before-function-paren": ["error", "always"], // 要求在function小括号左边设置空格，否则报错
+    "space-before-function-paren": ["off", "always"], // 关闭规则
+    "space-before-function-paren": ["off", "never"], // 关闭规则
+
+    "no-multiple-empty-lines": ["error", { max: 3 }], // 文件中间允许出现3个或以内的空白行
+    "no-multiple-empty-lines": ["off", { max: 3 }] // 关闭规则
   },
   parserOptions: {
     parser: "babel-eslint"
--- src/views/index/index.vue
+++ src/views/index/index.vue
@@ -1,7 +1,5 @@
 <template>
-  <div id="app">
-    <router-view />
-  </div>
+  <div id="app">sasaasdsdasdssdaaddd</div>
 </template>
 
 <style lang="less"></style>
